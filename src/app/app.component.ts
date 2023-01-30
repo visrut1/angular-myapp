48d42570a4f8c24d97a5740422e79c803a80cc72
visrut <visrutnavadiya111+github@gmail.com>
2023-01-30 11:25:21 +0530

    ENH: created form for adding pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Incubyte myapp';
+  pokemonForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.pokemonForm = fb.group({});
+  }
+
+  ngOnInit(): void {
+    this.pokemonForm = this.fb.group(
+      {
+        name: this.fb.control(''),
+        type: this.fb.control(''),
+        imgUrl: this.fb.control('')
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
